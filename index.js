0add71acd3b55dce4c7b9c54475eb1516feafe3b
volodymyr bondariev <vladmikhbond@hotmail.com>
2018-10-25 20:53:30 +0300

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+// z = z*z + c
+const IT = 40;
+let x1 = -2, y1 = -1, x2 = 1, y2 = 1;
+let d = 0.005;
+
+const canvas1 = document.getElementById("canvas1");
+const ctx = canvas1.getContext("2d");
+const size = canvas1.width;
+
+
+ctx.scale(canvas1.width /(x2 - x1), canvas1.height /(y2 - y1) );
+ctx.translate(-x1, -y1);
+
+for (let x = x1; x < x2; x += d) {
+    for (let y = y1; y < y2; y += d) {
+       let i = next(x, y);
+       if (i == IT)
+           ctx.fillRect(x, y, d, d);
+    }
+}
+
+
+function next(x, y) {
+    // Zn = Zn * Zn + c
+    let cx = x, cy = y;
+    for (let i = 0; i < IT; i++)
+    {
+        [x, y] = [x * x - y * y + cx, 2 * x * y + cy];
+        if ((x * x) + (y * y) > 4)
+            return i
+    }
+    return IT;
+}
