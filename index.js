2e04ac46a0a078ee3134e615325a0b5c2bf5e635
volodymyr bondariev <vladmikhbond@hotmail.com>
2018-10-28 10:19:46 +0200

    v 0.3 themas

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+let canvas1 = document.getElementById("canvas1");
+let iterText = document.getElementById("iterText");
+let colorSchemes = document.getElementsByName("colorSchemes");
+let info = document.getElementById("info");
+let iter = document.getElementById("iter");
+let iterButton = document.getElementById("iterButton");
+
 const K = 2;         // scale change for one step
 
 const ctx = canvas1.getContext("2d");
@@ -6,23 +13,15 @@ let x1 = -2, y1 = -1, x2 = 1, y2 = 1;
 let d = 1;
 let iterLimit = +iterText.value;
 const stack = [];
-const colors = ["red", "vermilion", "orange", "amber", "yellow",
-    "chartreuse", "green", "teal", "blue", "violet", "purple", "magenta"];
+let getColor = getColor0;
 
+restoreFromStorage();
 draw();
 
 // ---------------------- Drawing --------------------------------
 
 function draw() {
-    if (colorCheck.checked)
-        drawColor();
-    else
-        drawBW();
-    drawInfo();
-}
-
-function drawColor() {
-    ctx.fillStyle = "#222";
+    ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas1.width, canvas1.height );
 
     for (let x = 0; x < canvas1.width; x += d) {
@@ -35,20 +34,8 @@ function drawColor() {
             }
         }
     }
-}
+    drawInfo();
 
-function drawBW() {
-    ctx.fillStyle = "black";
-    ctx.clearRect(0, 0, canvas1.width, canvas1.height );
-
-    for (let x = 0; x < canvas1.width; x += d) {
-        for (let y = 0; y < canvas1.height; y += d) {
-            let [wx, wy] = canvasToWorld(x, y);
-            if (countIter(wx, wy) === iterLimit) {
-                ctx.fillRect(x, y, d, d);
-            }
-        }
-    }
 }
 
 function drawInfo() {
@@ -71,16 +58,6 @@ function countIter(cx, cy, limit = iterLimit) {
     }
     return limit;
 }
-// function countIter(x, y, limit = iterLimit) {
-//     let cx = x, cy = y;
-//     for (let i = 0; i < limit; i++)
-//     {
-//         [x, y] = [x * x - y * y + cx, 2 * x * y + cy];
-//         if ((x * x) + (y * y) > 4)
-//             return i
-//     }
-//     return limit;
-// }
 
 
 function canvasToWorld(canvasX, canvasY) {
@@ -93,6 +70,7 @@ function canvasToWorld(canvasX, canvasY) {
 
 canvas1.addEventListener('click', function (e) {
     save();
+    saveToStorage();
 
     let [x, y] = canvasToWorld(e.clientX, e.clientY);
     let dx = (x2 - x1) / K;
@@ -117,18 +95,25 @@ iterButton.addEventListener('click', function () {
     draw();
 });
 
-colorCheck.addEventListener('click', draw);
-
 
 canvas1.addEventListener('mousemove', function (e) {
     let [wx, wy] = canvasToWorld(e.clientX, e.clientY);
-    iter.innerHTML = countIter(wx, wy, 10000);
+    iter.innerHTML = countIter(wx, wy, 10000).toString();
 });
+
+
+colorSchemes[0].onclick = function() { getColor = getColor0; draw() };
+colorSchemes[1].onclick = function() { getColor = getColor1; draw() };
+colorSchemes[2].onclick = function() { getColor = getColor2; draw() };
+colorSchemes[3].onclick = function() { getColor = getColor3; draw() };
+
+
 
 // ---------------- Stack ----------------------
 
 function save() {
-    stack.push({"x1": x1, "x2": x2, "y1": y1, "y2": y2, "iterLimit":iterLimit });
+    let o = {"x1": x1, "x2": x2, "y1": y1, "y2": y2, "iterLimit":iterLimit };
+    stack.push(o);
 }
 
 function restore() {
@@ -137,9 +122,41 @@ function restore() {
     x1 = o.x1; x2 = o.x2; y1 = o.y1; y2 = o.y2; iterLimit = o.iterLimit;
 }
 
+function saveToStorage() {
+    let o = {"x1": x1, "x2": x2, "y1": y1, "y2": y2, "iterLimit":iterLimit };
+    localStorage.setItem("LAST", o);
+}
+
+function restoreFromStorage() {
+    let str = localStorage.getItem("LAST");
+
+    if (!str) return;
+    let o = JSON.parse(str);
+    x1 = o.x1; x2 = o.x2; y1 = o.y1; y2 = o.y2; iterLimit = o.iterLimit;
+}
+
+
 // ---------------- Colors ----------------------
 
-function getColor(n) {
+function getColor0(n) {
+    return "aqua";
+}
+
+function getColor1(n) {
+    const colors = ["red", "vermilion", "orange", "amber", "yellow",
+        "chartreuse", "green", "teal", "blue", "violet", "purple", "magenta"];
     let i = (n % colors.length);
     return colors[i];
+}
+
+function getColor2(n) {
+    const colors = ["red", "yellow"];
+    let i = (n % colors.length);
+    return colors[i];
+}
+
+function getColor3(n) {
+    let c = 256 + (n - iterLimit);
+    if (c < 0) c = 0;
+    return `rgb(${128}, ${c}, ${128})`;
 }
