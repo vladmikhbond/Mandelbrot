78156d7535d63b424d7e99cee11f29f4f4d1f095
volodymyr bondariev <vladmikhbond@hotmail.com>
2018-10-28 16:12:58 +0200

    v 0.3 themas

--- index.js
+++ index.js
@@ -1,36 +1,51 @@
-let canvas1 = document.getElementById("canvas1");
-let iterText = document.getElementById("iterText");
-let colorSchemes = document.getElementsByName("colorSchemes");
-let info = document.getElementById("info");
-let iter = document.getElementById("iter");
-let iterButton = document.getElementById("iterButton");
+const canvas1 = document.getElementById("canvas1");
+const iterText = document.getElementById("iterText");
+const colorSchemes = document.getElementsByName("colorSchemes");
+const info = document.getElementById("info");
+const iter = document.getElementById("iter");
+const iterButton = document.getElementById("iterButton");
+const resetButton = document.getElementById("resetButton");
+const darkColor = document.getElementById("darkColor");
+const lightColor = document.getElementById("lightColor");
+const thirdColor = document.getElementById("thirdColor");
 
-const K = 2;         // scale change for one step
-
+const D = 1;      // canvas pixel
+const K = 2;      // scale change for one step
 const ctx = canvas1.getContext("2d");
 
-let x1 = -2, y1 = -1, x2 = 1, y2 = 1;
-let d = 1;
-let iterLimit = +iterText.value;
-const stack = [];
-let getColor = getColor0;
-
+let x1, x2, y1, y2;
+let iterLimit;
+let stack;
+let getColor;
+//
+init();
 restoreFromStorage();
 draw();
 
-// ---------------------- Drawing --------------------------------
+function init() {
+    x1 = -2; y1 = -1; x2 = 1; y2 = 1;
+    iterLimit = +iterText.value;
+    stack = [];
+    getColor = getColor0;
+    iterLimit = 100;
+    //
+    iterText.value = iterLimit;
+    colorSchemes[0].checked = true;
+
+    draw();
+}
 
 function draw() {
-    ctx.fillStyle = "black";
+    ctx.fillStyle = darkColor.value;
     ctx.fillRect(0, 0, canvas1.width, canvas1.height );
 
-    for (let x = 0; x < canvas1.width; x += d) {
-        for (let y = 0; y < canvas1.height; y += d) {
+    for (let x = 0; x < canvas1.width; x += D) {
+        for (let y = 0; y < canvas1.height; y += D) {
             let [wx, wy] = canvasToWorld(x, y);
             let count = countIter(wx, wy);
             if (count < iterLimit) {
                 ctx.fillStyle = getColor(count);
-                ctx.fillRect(x, y, d, d);
+                ctx.fillRect(x, y, D, D);
             }
         }
     }
@@ -90,17 +105,13 @@ canvas1.addEventListener("contextmenu", function (e) {
     draw();
 });
 
-iterButton.addEventListener('click', function () {
-    iterLimit = +iterText.value;
-    draw();
-});
-
-
 canvas1.addEventListener('mousemove', function (e) {
     let [wx, wy] = canvasToWorld(e.clientX, e.clientY);
     iter.innerHTML = countIter(wx, wy, 10000).toString();
 });
 
+
+resetButton.addEventListener('click', init);
 
 colorSchemes[0].onclick = function() { getColor = getColor0; draw() };
 colorSchemes[1].onclick = function() { getColor = getColor1; draw() };
@@ -109,9 +120,19 @@ colorSchemes[3].onclick = function() { getColor = getColor3; draw() };
 colorSchemes[4].onclick = function() { getColor = getColor4; draw() };
 colorSchemes[5].onclick = function() { getColor = getColor5; draw() };
 
+darkColor.oninput = draw;
+lightColor.oninput = draw;
+thirdColor.oninput = function() {
+    level = +("0x" + this.value.slice(1, 3));
+    draw()
+};
 
+iterButton.addEventListener('click', function () {
+    iterLimit = +iterText.value;
+    draw();
+});
 
-// ---------------- Stack ----------------------
+// ---------------- Stack & Storage ----------------------
 
 function save() {
     let o = {"x1": x1, "x2": x2, "y1": y1, "y2": y2, "iterLimit":iterLimit };
@@ -125,9 +146,9 @@ function restore() {
 }
 
 
-
 function saveToStorage() {
-    let o = {"x1": x1, "x2": x2, "y1": y1, "y2": y2, "iterLimit":iterLimit };
+    let o = {"x1": x1, "x2": x2, "y1": y1, "y2": y2,
+        "iterLimit": iterLimit, "stack": stack};
     localStorage.setItem("LAST", JSON.stringify(o));
 }
 
@@ -135,7 +156,12 @@ function restoreFromStorage() {
     let str = localStorage.getItem("LAST");
     try {
         let o = JSON.parse(str);
-        x1 = o.x1; x2 = o.x2; y1 = o.y1; y2 = o.y2; iterLimit = o.iterLimit;
+        x1 = o.x1; x2 = o.x2; y1 = o.y1; y2 = o.y2;
+        iterLimit = o.iterLimit;
+        stack = o.stack;
+        //
+        iterText.value = iterLimit;
+        colorSchemes[0].checked = true;
     }
     catch {}
 }
@@ -144,7 +170,7 @@ function restoreFromStorage() {
 // ---------------- Colors ----------------------
 
 function getColor0() {
-    return "aqua";
+    return lightColor.value;
 }
 
 function getColor1(n) {
@@ -155,24 +181,27 @@ function getColor1(n) {
 }
 
 function getColor2(n) {
-    const colors = ["red", "yellow"];
+    const colors = [lightColor.value, thirdColor.value];
     let i = (n % colors.length);
     return colors[i];
 }
-const l = 100;
+
+let level = 64;
 
 function getColor3(n) {
     let c = 256 + 2 * (n - iterLimit);
-    if (c < l) c = l;
-    return `rgb(${c}, ${l}, ${l})`;
+    if (c < level) c = level;
+    return `rgb(${c}, ${level}, ${level})`;
 }
+
 function getColor4(n) {
     let c = 256 + 2 * (n - iterLimit);
-    if (c < l) c = l;
-    return `rgb(${l}, ${c}, ${l})`;
+    if (c < level) c = level;
+    return `rgb(${level}, ${c}, ${level})`;
 }
+
 function getColor5(n) {
     let c = 256 + 2 * (n - iterLimit);
-    if (c < l) c = l;
-    return `rgb(${l}, ${l}, ${c})`;
+    if (c < level) c = level;
+    return `rgb(${level}, ${level}, ${c})`;
 }
