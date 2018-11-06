1b40fd202afce34575bf5da41251c2597e15676f
volodymyr bondariev <vladmikhbond@hotmail.com>
2018-11-06 16:07:36 +0200

    v 0.41 exp-imp

--- js/math.js
+++ js/math.js
@@ -2,7 +2,7 @@
 
 // Zn = Zn * Zn + c;
 // Zo = 0; c = {x, y}
-function countIter(cx, cy, limit = iterLimit) {
+function countIter1(cx, cy, limit = iterLimit) {
     let x = cx, y = cy;
     for (let i = 0; i < limit; i++)
     {
@@ -19,4 +19,28 @@ function canvasToWorld(canvasX, canvasY) {
         canvasX * (x2 - x1) / canvas1.width + x1,
         canvasY * (y2 - y1) / canvas1.height + y1 ]
 }
+
+
+function countIter(cx, cy, limit = iterLimit, pow = 6) {
+    let x = cx, y = cy;  // первая итерация z = c
+    for (let i = 0; i < limit; i++)
+    {
+        // ---- z2 = z**p + c
+        let xs = x, ys = y;
+        for (let p = pow; p > 1; p--) {
+            [xs, ys] = [xs * x - ys * y, x * y + xs * ys];
+        }
+        x = xs + cx;
+        y = ys + cy;
+        // ------------------
+        if ((x * x) + (y * y) > 4)
+            return i
+    }
+    return limit;
+}
+
+// let iterLimit = 1000;
+// for (let x = -2; x < 1; x += 0.1) {
+//     console.log(countIter1(x, 0),  countIter(x, 0))
+// }
 
